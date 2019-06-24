58c4e75e6e048eec41ea497fbc99637c3e2078f2
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-24 20:29:22 +0200

    building Retrofit GsonConverterFactory

--- app/src/main/java/com/example/myapplication_retrofitmaze/MainActivity.java
+++ app/src/main/java/com/example/myapplication_retrofitmaze/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
     private TextView textViewResult;
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        textViewResult = findViewById(R.id.text_view_result);
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
 }
