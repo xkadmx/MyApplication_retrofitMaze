ca6cce1d8518a92a602906c9a5b4952ebc6048b4
xkadmx <zuzanna.rosinska@yahoo.com>
2019-06-25 11:46:32 +0200

    creating a list of posts from the web service into an application (it appears in a TextView

--- app/src/main/java/com/example/myapplication_retrofitmaze/MainActivity.java
+++ app/src/main/java/com/example/myapplication_retrofitmaze/MainActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -25,5 +30,35 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         JsonPlaceHolderAPI jsonPlaceHolderAPI = retrofit.create(JsonPlaceHolderAPI.class);
+
+        Call<List<Post>> call = jsonPlaceHolderAPI.getPosts();
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+                if(!response.isSuccessful()){
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+
+                List  <Post> posts = response.body();
+                for(Post post : posts){
+                    String content ="";
+                    content += "ID: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId() + "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+
+                    textViewResult.append(content);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+
+            }
+        });
     }
 }
